955277cc1e942f1df03c3143e72cf77ce9ab3ae3
Lukasz Dobosz <lukas.dobosz@capgemini.com>
2019-07-07 17:19:00 +0100

    poasdf

--- 00-zmienne.tf
+++ 00-zmienne.tf
@@ -1,5 +1,12 @@
 
-variable "regionwylacz" {
-   default = "eu-west-2"
+variable "region" {
+   default = "eu-west-1"
    description = "to tam gdzie ma byc zbudowany system"
 }
+
+
+variable "azs" {
+   type = "list"
+   default = ["${var.region}a", "${var.region}b", "${var.region}c" ]
+}
+
