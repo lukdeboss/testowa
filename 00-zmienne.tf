f11989fea28d349f4b8fa95be73aae0a10091570
Lukasz Dobosz <lukas.dobosz@capgemini.com>
2019-07-07 17:34:07 +0100

    adsfasdf

--- 00-zmienne.tf
+++ 00-zmienne.tf
@@ -4,8 +4,14 @@ variable "region" {
    description = "to tam gdzie ma byc zbudowany system"
 }
 
+data "aws_availability_zones" "azs" {
+}
+
+
 
 locals {
    azs = ["${var.region}a", "${var.region}b", "${var.region}c" ]
+   subnets_public  = [ "10.1.10.0/24", "10.2.10.0/24", "10.3.10.0/24" ]
+   azs2 = data.aws_availability_zones.azs.names
 }
 
--- 20-vpc.tf
+++ 20-vpc.tf
@@ -5,6 +5,11 @@
 # Create a VPC
 resource "aws_vpc" "vpc1" {
   cidr_block = "10.0.0.0/16"
+  # instance_tenancy = "default"
+  # tags {
+  #    jakas = "taka"
+  #    inna  = "calkowicie"
+  # }
 }
 
 
